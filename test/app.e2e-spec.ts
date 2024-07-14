1dc31e18ce29e74d11d47818729a2d013f66080a
Sushant Rajbanshi <sushant.rajbanshi@webo.digital>
2024-07-14 09:45:34 +0545

    chore: Update Bruno API scripts

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,40 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+import request from "supertest";
+import { AppModule } from "./../src/app.module";
 
-describe('AppController (e2e)', () => {
+describe("AppController (e2e)", () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it("/ (GET)", () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get("/ninjas")
       .expect(200)
-      .expect('Hello World!');
+      .expect([
+        {
+          id: 0,
+          name: "Ryu",
+          belt: "Orange",
+        },
+        {
+          id: 1,
+          name: "Jin",
+          belt: "Black",
+        },
+      ]);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
